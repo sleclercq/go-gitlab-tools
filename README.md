683dc8f88481c7d5a043b4d6169bb1532d599f61
Stephane Leclercq <leclercq@ekino.com>
2016-06-14 17:43:54 +0200

    Enable variables settings

---
+++ README.md
@@ -0,0 +1,10 @@
+go-gitlab-tools
+================
+
+go-gitlab-tools is a simple suite of tools written in golang to consume gitlab API and perform maintenance operations on my personal and professional projects.
+
+[![Build Status](https://travis-ci.org/sleclercq/go-gitlab-tools.png?branch=master)](https://travis-ci.org/sleclercq/go-gitlab-tools)
+
+
+
+
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/plouc/go-gitlab-client"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Host                string `json:"host"`
+	ApiPath             string `json:"api_path"`
+	Token               string `json:"token"`
+	PublishingBaseUrl   string `json:"publishing_base_url"`
+	PublishingLogin     string `json:"publishing_login"`
+	PublishingPassword  string `json:"publishing_password"`
+	FlowdockSourceToken string `json:"flowdock_source_token"`
+}
+
+func main() {
+	help := flag.Bool("help", false, "Show usage")
+
+	file, e := ioutil.ReadFile("config.json")
+	if e != nil {
+		fmt.Printf("Config file error: %v\n", e)
+		os.Exit(1)
+	}
+
+	var config Config
+	json.Unmarshal(file, &config)
+	fmt.Printf("Results: %+v\n", config)
+
+	gitlab := gogitlab.NewGitlab(config.Host, config.ApiPath, config.Token)
+
+	var method string
+	flag.StringVar(&method, "m", "", "Specify method, available methods:\n"+
+		"  > -m variables -id PROJECT_ID")
+
+	var id string
+	flag.StringVar(&id, "id", "", "Specify project id")
+
+	flag.Usage = func() {
+		fmt.Printf("Usage:\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	if *help == true || method == "" {
+		flag.Usage()
+		return
+	}
+
+	startedAt := time.Now()
+	defer func() {
+		fmt.Printf("processed in %v\n", time.Now().Sub(startedAt))
+	}()
+
+	switch method {
+
+	case "variables":
+
+		if id == "" {
+			flag.Usage()
+			return
+		}
+
+		fmt.Println("Setting project variables...")
+
+		addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingBaseUrl", config.PublishingBaseUrl)
+		addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingLogin", config.PublishingLogin)
+		addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingPassword", config.PublishingPassword)
+		addVariable(gitlab, id, "FLOWDOCK_SOURCE_TOKEN", config.FlowdockSourceToken)
+	}
+}
+
+func addVariable(gitlab *gogitlab.Gitlab, id string, key string, value string) (*gogitlab.Variable, error) {
+	fmt.Printf("Setting project %s variable %s -> %s\n", id, key, value)
+
+	variable := gogitlab.Variable{
+		Key:   key,
+		Value: value,
+	}
+
+	result, err := gitlab.AddProjectVariable(id, &variable)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return result, err
+}
