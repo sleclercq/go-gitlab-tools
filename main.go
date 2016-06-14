afb948e4ad39f48de09ba190dc9a35e41b21f017
Stephane Leclercq <leclercq@ekino.com>
2016-06-14 18:40:23 +0200

    feat(ci) setup ci with runners and variables

--- main.go
+++ main.go
@@ -37,7 +37,10 @@ func main() {
 
 	var method string
 	flag.StringVar(&method, "m", "", "Specify method, available methods:\n"+
-		"  > -m variables -id PROJECT_ID")
+		"  > -m setupci -id PROJECT_ID\n"+
+		"  > -m variables -id PROJECT_ID\n"+
+		"  > -m runner -id PROJECT_ID\n"+
+	"")
 
 	var id string
 	flag.StringVar(&id, "id", "", "Specify project id")
@@ -60,6 +63,21 @@ func main() {
 
 	switch method {
 
+	case "setupci":
+
+		if id == "" {
+			flag.Usage()
+			return
+		}
+
+		fmt.Println("Setup project CI...")
+
+		addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingBaseUrl", config.PublishingBaseUrl)
+		addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingLogin", config.PublishingLogin)
+		addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingPassword", config.PublishingPassword)
+		addVariable(gitlab, id, "FLOWDOCK_SOURCE_TOKEN", config.FlowdockSourceToken)
+		enableSharedRunners(gitlab, id)
+
 	case "variables":
 
 		if id == "" {
@@ -73,7 +91,30 @@ func main() {
 		addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingLogin", config.PublishingLogin)
 		addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingPassword", config.PublishingPassword)
 		addVariable(gitlab, id, "FLOWDOCK_SOURCE_TOKEN", config.FlowdockSourceToken)
+
+	case "runner":
+		if id == "" {
+			flag.Usage()
+			return
+		}
+
+		enableSharedRunners(gitlab, id)
 	}
+}
+
+func enableSharedRunners(gitlab *gogitlab.Gitlab, id string) (*gogitlab.Project, error) {
+	fmt.Printf("Enabling shared runners on project %s", id)
+
+	project := gogitlab.Project{
+		SharedRunners: true,
+	}
+
+	result, err := gitlab.UpdateProject(id, &project)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return result, err
 }
 
 func addVariable(gitlab *gogitlab.Gitlab, id string, key string, value string) (*gogitlab.Variable, error) {
