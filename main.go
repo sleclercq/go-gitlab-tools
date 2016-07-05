06a936544088c6874a6068cabbc59a824abfc488
Stephane Leclercq <leclercq@ekino.com>
2016-07-05 17:41:39 +0200

    feat(project) add create project

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/plouc/go-gitlab-client"
 	"io/ioutil"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -40,10 +41,17 @@ func main() {
 		"  > -m setupci -id PROJECT_ID\n"+
 		"  > -m variables -id PROJECT_ID\n"+
 		"  > -m runner -id PROJECT_ID\n"+
-	"")
+		"  > -m create -name PROJECT_NAME -namespace_id PROJECT_NAMESPACE\n"+
+		"")
 
 	var id string
 	flag.StringVar(&id, "id", "", "Specify project id")
+
+	var name string
+	flag.StringVar(&name, "name", "", "Specify project name")
+
+	var namespaceId string
+	flag.StringVar(&namespaceId, "namespace_id", "", "Specify project namespace id")
 
 	flag.Usage = func() {
 		fmt.Printf("Usage:\n")
@@ -63,6 +71,21 @@ func main() {
 
 	switch method {
 
+	case "create":
+		if name == "" || namespaceId == "" {
+			flag.Usage()
+			return
+		}
+
+		namespaceIdInt, _ := strconv.Atoi(namespaceId)
+		project, err := createProject(gitlab, name, namespaceIdInt)
+
+		if err != nil {
+			return
+		}
+
+		setupCi(gitlab, strconv.Itoa(project.Id), config)
+
 	case "setupci":
 
 		if id == "" {
@@ -72,11 +95,7 @@ func main() {
 
 		fmt.Println("Setup project CI...")
 
-		addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingBaseUrl", config.PublishingBaseUrl)
-		addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingLogin", config.PublishingLogin)
-		addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingPassword", config.PublishingPassword)
-		addVariable(gitlab, id, "FLOWDOCK_SOURCE_TOKEN", config.FlowdockSourceToken)
-		enableSharedRunners(gitlab, id)
+		setupCi(gitlab, id, config)
 
 	case "variables":
 
@@ -85,12 +104,7 @@ func main() {
 			return
 		}
 
-		fmt.Println("Setting project variables...")
-
-		addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingBaseUrl", config.PublishingBaseUrl)
-		addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingLogin", config.PublishingLogin)
-		addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingPassword", config.PublishingPassword)
-		addVariable(gitlab, id, "FLOWDOCK_SOURCE_TOKEN", config.FlowdockSourceToken)
+		setupProjectVariables(gitlab, id, config)
 
 	case "runner":
 		if id == "" {
@@ -102,10 +116,70 @@ func main() {
 	}
 }
 
-func enableSharedRunners(gitlab *gogitlab.Gitlab, id string) (*gogitlab.Project, error) {
-	fmt.Printf("Enabling shared runners on project %s", id)
+func createProject(gitlab *gogitlab.Gitlab, name string, namespaceId int) (*gogitlab.Project, error) {
+	fmt.Printf("Creating project with name %v and namespace %v\n", name, namespaceId)
 
 	project := gogitlab.Project{
+		Name:                 name,
+		NamespaceId:          namespaceId,
+		MergeRequestsEnabled: true,
+		PublicBuilds:         false,
+	}
+
+	result, err := gitlab.CreateProject(&project)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Printf("Created project with id %v and url %v\n", result.Id, result.WebUrl)
+
+	return result, err
+}
+
+func setupCi(gitlab *gogitlab.Gitlab, id string, config Config) (*gogitlab.Project, error) {
+	err := setupProjectVariables(gitlab, id, config)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	result, err := enableSharedRunners(gitlab, id)
+	if err != nil {
+		fmt.Println(err.Error())
+		return result, err
+	}
+
+	return result, err
+}
+
+func setupProjectVariables(gitlab *gogitlab.Gitlab, id string, config Config) error {
+	fmt.Println("Setting project variables...")
+
+	_, err := addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingBaseUrl", config.PublishingBaseUrl)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	_, err = addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingLogin", config.PublishingLogin)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	_, err = addVariable(gitlab, id, "ORG_GRADLE_PROJECT_publishingPassword", config.PublishingPassword)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	_, err = addVariable(gitlab, id, "FLOWDOCK_SOURCE_TOKEN", config.FlowdockSourceToken)
+	return err
+}
+
+func enableSharedRunners(gitlab *gogitlab.Gitlab, id string) (*gogitlab.Project, error) {
+	fmt.Printf("Enabling shared runners on project %v\n", id)
+
+	project := gogitlab.Project{
+		BuildsEnabled: true,
+		PublicBuilds:  false,
 		SharedRunners: true,
 	}
 
